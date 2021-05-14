967a96e92cdc215f61e4b8f48d78497534f9e1c6
Ramon <ramm050489@gmail.com>
2021-05-14 18:47:00 -0400

    commit tree 6ca2c71d4713add7f0a1bfc77838536b1d96076e

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+function findMatching(driversNames,string) {
+    return driversNames.filter( possibleMatch =>
+        possibleMatch.toLowerCase() === string.toLowerCase()
+        )
+}
+
+function fuzzyMatch(driversNames,string) {
+    return driversNames.filter( possibleMatch =>
+        possibleMatch.toLowerCase().indexOf(string.toLowerCase()) === 0
+        )
+}
+
+function matchName(driver, string) {
+    return driver.filter(result => result.name === string)
+}
